077ce82c73c5e78d55ae80fe6aee148227d3de14
Emanuel Grancho <egrancho>
2022-01-05 00:58:27 +0000

    First simple concepts and approach.

--- TaskTimer/Program.cs
+++ TaskTimer/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TasksLibrary;
+
+DateTime estimatedTimeToFinishTask = DateTime.Now.AddSeconds(5);
+List<string> whatToDo = new List<string>() { "Task A","Task B","Task C","Task D"};
+
+TaskTimer taskTimer = new TaskTimer("projectA", estimatedTimeToFinishTask, whatToDo);
+
+taskTimer.Start();
+
+Thread.Sleep(2000);
+
+taskTimer.Stop();
+
+Console.WriteLine($"Estimated date: {estimatedTimeToFinishTask}");
+Console.WriteLine($"Duration: {taskTimer.TaskEvaluation().TotalSeconds} s");
---
+++ TasksLibrary/TaskTimer.cs
@@ -0,0 +1,34 @@
+﻿namespace TasksLibrary
+{
+    public class TaskTimer
+    {
+        private static string _projectName;
+        private static DateTime _taskEndDate;
+        private static List<string> _whatToDo;
+
+        private static DateTime _startDate;
+        private static DateTime _endDate;
+
+        public TaskTimer(string projectName, DateTime taskEndDate, List<string> whatToDo)
+        {
+            _whatToDo = new List<string>(whatToDo);
+            _taskEndDate = taskEndDate;
+            _projectName = projectName;
+        }
+
+        public void Start()
+        {
+            _startDate = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            _endDate = DateTime.Now;
+        }
+
+        public TimeSpan TaskEvaluation()
+        {
+            return _endDate - _startDate;
+        }
+    }
+}
